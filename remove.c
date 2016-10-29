76682bef2f78171e9748c67381dd51efdd4c71d4
mucahiderenler <mucahid.erenler@gmail.com>
2016-10-29 14:58:54 +0200

    comment lines added

--- remove.c
+++ remove.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int m_option = 0;
-int M_option = 0;
-int d_option = 0;
+int m_option = 0; //lower char
+int M_option = 0; //upper char
+int d_option = 0; //digits
 
 void file_remove(FILE *fp){
     int c;
@@ -13,7 +13,7 @@ void file_remove(FILE *fp){
              if(!(m_option || d_option)){
                 if(!(islower(c) || isdigit(c)))
                     putchar(c);
-            }
+            }                                         // choosing the character and remove.
              if(!(M_option || d_option)){
                 if(!(isdigit(c) || isupper(c)))
                     putchar(c);
@@ -23,17 +23,17 @@ void file_remove(FILE *fp){
                     putchar(c);
             } 
         }   
-    fclose(fp);
+            fclose(fp);
 }
 
 int main(int argc, char* argv[]){
     FILE *ilp;
    
-    if(argc>1){
-    if(argv[1][0] == '-'){
+    if(argc>1){				//check if any command given
+        if(argv[1][0] == '-'){
             if(argv[1][1] == 'm')
                 m_option = 1;
-            else if (argv[1][1] == 'M')
+            else if (argv[1][1] == 'M')  // chech which command given
                 M_option = 1;
             else if(argv[1][1] == 'd')
                 d_option = 1;
@@ -42,15 +42,13 @@ int main(int argc, char* argv[]){
         printf("use '-' for command\n");
         return EXIT_FAILURE;
     }
-
-    for(int i =2 ; i<argc; i++){
-         if((ilp = fopen(argv[i],"r")) != NULL)
-             file_remove(ilp);
-         else{
-             printf("\ni could not read this file %s\n",argv[i]);
-         }       
-        }
-        
-    } else
-        printf("You didn't give any commands.\n");    
-}
+    for(int i =2 ; i<argc; i++){                    
+         if((ilp = fopen(argv[i],"r")) != NULL)    // open all the files that given
+             file_remove(ilp);                     // remove the chosen one
+         else
+             printf("\ni could not read this file %s\n",argv[i]);                
+      }
+         
+   } else
+        printf("You didn't give any commands.\n");   
+ }
