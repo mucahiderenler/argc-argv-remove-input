adc5ca8c4d5c973e88fd4b956bd152bd006a0b59
mucahiderenler <mucahid.erenler@gmail.com>
2016-10-31 04:30:21 +0100

    redefine the array and their values

--- permitation.c
+++ permitation.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+char newstring [];
 
 char * swtch(char word [],int x,int y){
-    char * newstring = word;
+ 
+    for(int i = 0;i<strlen(word);i++){
+        newstring[i] = word[i];    
+    }
     newstring[x] = newstring[y];
     newstring[y] = word[x];
     return newstring;   
 }
 
 void permute(char word [], int place){
-    if(place == sizeof(*word)-1){
-        printf("%s",word);
+    if(place == strlen(word)-1){
+        printf("%s\n",word);
     }
-    for(int nextchar = place; nextchar<sizeof(*word);nextchar++){
+    for(int nextchar = place; nextchar<strlen(word);nextchar++){
         permute(swtch(word,place,nextchar),place+1);
     }
 }
